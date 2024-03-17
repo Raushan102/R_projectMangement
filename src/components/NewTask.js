15a697564323a93bfdfe72ed445462f4437cc74b
Raushan kumar saw <124686068+Raushan102@users.noreply.github.com>
2024-03-17 22:05:32 +0530

    last commit

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,34 +1,29 @@
 import React from "react";
-import Model from './Model'
-import { useRef,useState } from "react";
+
+import { useRef, useState } from "react";
+import Model from "./Model";
 
 function NewTask({ Add }) {
-
-
-  let model=useRef()
+  let model = useRef();
 
   const [interTaskName, setInterTaskName] = useState();
   function TaskInterDataHandler(event) {
-
-    if(event.target.value==='')
-    {
-        model.current.Open()
-    }
-    else{
-      setInterTaskName(event.target.value);
-    }
-   
+    setInterTaskName(event.target.value);
   }
 
   function addTaskHandler() {
-    Add(interTaskName);
-    setInterTaskName("");
+    if (interTaskName === "") {
+      model.current.Open();
+    } else {
+      Add(interTaskName);
+      setInterTaskName("");
+    }
   }
 
   return (
     <>
-       <Model ref={model}>
-       <h2 className=" border-l text-stone-900 text-xl my-1 font-black">
+      <Model ref={model} Title="Okay">
+        <h2 className=" border-l text-stone-900 text-xl my-1 font-black">
           Invalid input
         </h2>
         <p className="text-stone-600 my-1">
@@ -37,7 +32,7 @@ function NewTask({ Add }) {
         <p className="text-stone-600 my-1">
           please make sure you provide a valid value for task name
         </p>
-       </Model>
+      </Model>
       <div className="flex items-center ">
         <input
           type="text"
